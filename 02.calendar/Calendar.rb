a49135467135c853cddb330cb9da7507a3344bc7
guchi5 <zero19390901@gmail.com>
2023-12-02 22:58:00 +0900

    day_listのnil部分までの修正 - day_listにnilを代入していた個所を削除   - 次にやること      - create_calの戻り値の修正

--- 02.calendar/Calendar.rb
+++ 02.calendar/Calendar.rb
@@ -6,42 +6,40 @@ require 'date'
 WEEK_NUM = 7
 MONTH_NUM = 12
 
-def createCal(year, month)
-  week_list = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+def create_cal(year, month)
+  week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
   f_cal = Date.new(year, month, 1)
   l_cal = Date.new(year, month, -1)
-  day_num = (l_cal-f_cal).to_i
-  space_count = week_list.index(f_cal.strftime("%a"))+1
+  day_num = (l_cal - f_cal).to_i
+  space_count = week.index(f_cal.strftime("%a"))+1
   sat_count = 0
-  day_list = Array.new(day_num+space_count)
+  day_list = Array.new(day_num + space_count)
   day_list.each_with_index{|value, day|
     if space_count > day+1
-      day_list[day] = nil
       next
     end
-    day_list[day] = (day+2)-(space_count)
-    if (f_cal + (day+1)-(space_count)).strftime("%a") == week_list[6]
+    day_list[day] = (day + 2) - space_count
+    if (f_cal + (day + 1) - (space_count)).strftime("%a") == week[6]
       sat_count += 1
     end
   }
   return [[f_cal.year, f_cal.month, sat_count], day_list]
-
 end
 
-def showCal(cal_list)
+def show_cal(cal_list)
   cal = Date.new(cal_list[0][0], cal_list[0][1], 1)
   sat_count = cal_list[0][2]
   today = Date.today
   day_list = cal_list[1]
-  header = cal.strftime('%B')+"\s"+cal.strftime("%Y")
+  header = cal.strftime('%B') + "\s" + cal.strftime("%Y")
 
-  for i in 0..(((20-(header.length))/2)-1)
+  for i in 0..(((20 - (header.length)) / 2) - 1)
     print("\s")
   end
   print(header)
   print("\n")
   puts("Su Mo Tu We Th Fr Sa")
-  day_list.each_with_index{|day, index|
+  day_list.each_with_index do |day, index|
     if(day==nil)
       print("\s\s\s")
       next
@@ -54,10 +52,10 @@ def showCal(cal_list)
     else
       printf("%d\s", day)
     end
-    if((index+1)%WEEK_NUM == 0)
+    if((index + 1) % WEEK_NUM == 0)
       print("\n")
     end
-  }
+  end
   if(sat_count < 5)
     print("\n")
   end
@@ -75,5 +73,5 @@ if(month == nil)
 end
 year = year.to_i
 month = month.to_i
-cal = createCal(year, month)
-showCal(cal)
+cal = create_cal(year, month)
+show_cal(cal)
