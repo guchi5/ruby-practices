6268420d7efe7297b4b99bf4365095b31396d5a8
guchi5 <zero19390901@gmail.com>
2023-12-02 23:03:07 +0900

    演算子とifの空白を修正

--- 02.calendar/Calendar.rb
+++ 02.calendar/Calendar.rb
@@ -14,7 +14,7 @@ def create_cal(year, month)
   space_count = week.index(f_cal.strftime("%a"))+1
   sat_count = 0
   day_list = Array.new(day_num + space_count)
-  day_list.each_with_index{|value, day|
+  day_list.each_with_index do |value, day|
     if space_count > day+1
       next
     end
@@ -22,7 +22,7 @@ def create_cal(year, month)
     if (f_cal + (day + 1) - (space_count)).strftime("%a") == week[6]
       sat_count += 1
     end
-  }
+  end
   return [[f_cal.year, f_cal.month, sat_count], day_list]
 end
 
@@ -40,23 +40,23 @@ def show_cal(cal_list)
   print("\n")
   puts("Su Mo Tu We Th Fr Sa")
   day_list.each_with_index do |day, index|
-    if(day==nil)
+    if (day==nil)
       print("\s\s\s")
       next
     end
-    if(day<10)
+    if (day<10)
       print("\s")
     end
-    if (day == today.day && cal_list[0][0]==today.year && cal_list[0][1]==today.month)
+    if (day == today.day && cal_list[0][0] == today.year && cal_list[0][1] == today.month)
       print("\e[7m#{day}\e[0m\s")
     else
       printf("%d\s", day)
     end
-    if((index + 1) % WEEK_NUM == 0)
+    if ((index + 1) % WEEK_NUM == 0)
       print("\n")
     end
   end
-  if(sat_count < 5)
+  if (sat_count < 5)
     print("\n")
   end
   print("\n")
@@ -65,10 +65,10 @@ end
 params = ARGV.getopts("m:", "y:")
 year = params["y"]
 month = params["m"]
-if(year == nil)
+if (year == nil)
   year = Date.today.year
 end
-if(month == nil)
+if (month == nil)
   month = Date.today.month
 end
 year = year.to_i
