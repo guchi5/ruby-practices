69719c2ee8983022ca8f5ebe3dc7a3ec8c457e61
guchi5 <zero19390901@gmail.com>
2023-12-05 16:00:28 +0900

    rubocopによる修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,10 +2,10 @@
 
 # フレーム単位のリストを返す
 def create_frames
-  input = ARGV[0].split(",")
+  input = ARGV[0].split(',')
   frames = [] # フレーム単位のリスト
 
-  while input.length > 0 do
+  while input.length.positive?
     # 10回目は全ての得点を代入して終了
     if frames.length == 9
       frames << input.dup
@@ -14,11 +14,11 @@ def create_frames
 
     frame = []
     frame << input.shift
-    if frame[0] == "X"
-      frame << "0"
-    else
-      frame << input.shift
-    end
+    frame << if frame[0] == 'X'
+               '0'
+             else
+               input.shift
+             end
     frames << frame
   end
   frames
@@ -26,7 +26,7 @@ end
 
 # フレームごとのスコアの合計値を返す
 def sum_frame_score(frame)
-  frame.sum {|score| score == "X" ? 10 : score.to_i}
+  frame.sum { |score| score == 'X' ? 10 : score.to_i }
 end
 
 # 全てのフレームのスコアの合計値を返す
@@ -36,20 +36,16 @@ def calculate_total_score(frames)
     score += sum_frame_score(frame)
 
     # 10回目の場合は終了
-    if index == 9
-      break
-    end
+    break if index == 9
 
     # ストライクの場合
-    if frame[0] == "X"
+    if frame[0] == 'X'
       score += sum_frame_score(frames[index + 1][0, 2])
-      if frames[index + 1][0] == "X" && index + 1 != 9    # 次のフレームがストライクの場合は、さらに次のフレームの1投目のスコアを加算
-        score += sum_frame_score(frames[index + 2][0, 1])
-      end
+      score += sum_frame_score(frames[index + 2][0, 1]) if frames[index + 1][0] == 'X' && index + 1 != 9 # 次のフレームがストライクの場合は、さらに次のフレームの1投目のスコアを加算
 
     # スペアの場合
     elsif (frame[0].to_i + frame[1].to_i) == 10
-      score += sum_frame_score(frames[index+1][0, 1])
+      score += sum_frame_score(frames[index + 1][0, 1])
     end
   end
   score
