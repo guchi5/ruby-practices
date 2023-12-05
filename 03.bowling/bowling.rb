445eb30f912e70a53957a976aa1f1c8a2b8b50e4
guchi5 <zero19390901@gmail.com>
2023-12-05 15:03:06 +0900

    bowling.rbの実装

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+
+# フレーム単位のリストを返す
+def create_frames
+  input = ARGV[0].split(",")
+  frames = [] # フレーム単位のリスト
+
+  while input.length > 0 do
+    # 10回目は全ての得点を代入して終了
+    if frames.length == 9
+      frames << input.dup
+      break
+    end
+
+    frame = []
+    frame << input.shift
+    if frame[0] == "X"
+      frame << "0"
+    else
+      frame << input.shift
+    end
+    frames << frame
+  end
+  frames
+end
+
+# フレームごとのスコアの合計値を返す
+def sum_frame_score(frame)
+  frame.sum {|score| score == "X" ? 10 : score.to_i}
+end
+
+# 全てのフレームのスコアの合計値を返す
+def calculate_total_score(frames)
+  score = 0
+  frames.each_with_index do |frame, index|
+    score += sum_frame_score(frame)
+
+    # 10回目の場合は終了
+    if index == 9
+      break
+    end
+
+    # ストライクの場合
+    if frame[0] == "X"
+      score += sum_frame_score(frames[index + 1][0, 2])
+      if frames[index + 1][0] == "X" && index + 1 != 9    # 次のフレームがストライクの場合は、さらに次のフレームの1投目のスコアを加算
+        score += sum_frame_score(frames[index + 2][0, 1])
+      end
+
+    # スペアの場合
+    elsif (frame[0].to_i + frame[1].to_i) == 10
+      score += sum_frame_score(frames[index+1][0, 1])
+    end
+  end
+  score
+end
+
+frames = create_frames
+puts calculate_total_score(frames)
